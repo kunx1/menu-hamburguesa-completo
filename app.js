04cabffea5bec778f68052057bc253ce922bdb82
Rodrigo Lezana <75607037+kunx1@users.noreply.github.com>
2023-03-10 04:51:56 -0300

    primer c

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.menu-button')
+const nav = document.querySelector('.nav')
+
+const menu = document.querySelector('.menu-button')
+
+
+
+btn.addEventListener('click', ()=>{
+    nav.classList.toggle('activo')
+    menu.classList.toggle('activox')
+})
